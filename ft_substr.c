3ff60b136e60d72cbf2b171d867f5314020a6f73
Matteo <229449054+chauvinmatteo@users.noreply.github.com>
2025-11-07 11:52:16 +0100

    added ft_substr.c

---
+++ ft_substr.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mchauvin <mchauvin@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/07 11:14:21 by mchauvin          #+#    #+#             */
+/*   Updated: 2025/11/07 11:50:50 by mchauvin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+char *ft_substr(char const *s, unsigned int start, size_t len)
+{
+	unsigned int i;
+	char *ptr;
+
+	i = 0;
+	if (!s[0])
+		return (ft_strdup(""));
+	if (start >= ft_strlen(s))
+		return (ft_strdup(""));
+	if (len >= ft_strlen(s) - start)
+		len = ft_strlen(s) - start;
+	ptr = malloc(sizeof(char) * (len + 1));
+	if (!ptr)
+		return (NULL);
+	while (i < len)
+	{
+		ptr[i] = s[i + start];
+		i++;
+	}
+	ptr[i] = '\0';
+	return (ptr);
+}
+
