a3952ce28b941df63caacaa50f68f816b8e5e443
chauvinmatteo <chauvin.matteo1@gmail.com>
2025-11-07 15:32:42 +0100

    Added ft_strjoin

---
+++ ft_strjoin.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mchauvin <mchauvin@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/07 15:19:12 by mchauvin          #+#    #+#             */
+/*   Updated: 2025/11/07 15:32:21 by mchauvin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+char *ft_strjoin(char const *s1, char const *s2)
+{
+	char *dest;
+	int i;
+	int j;
+
+	i = 0;
+	j = 0;
+	dest = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (!dest)
+		return (NULL);
+	while (s1[i])
+	{
+		dest[i] = s1[i];
+		i++;
+	}
+	while (s2[j])
+	{
+		dest[i] = s2[j];
+		i++;
+		j++;
+	}	
+	dest[i] = '\0';
+	return  (dest);	
+}
