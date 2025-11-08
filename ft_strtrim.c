e83399ffa1099c27460af8353d621d7e21545c19
Matteo <229449054+chauvinmatteo@users.noreply.github.com>
2025-11-08 11:12:20 +0100

    Added ft_strtrim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,50 @@
 /*   By: mchauvin <mchauvin@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/07 15:33:06 by mchauvin          #+#    #+#             */
-/*   Updated: 2025/11/07 16:06:02 by mchauvin         ###   ########.fr       */
+/*   Updated: 2025/11/08 11:11:57 by mchauvin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
+
+static int 	ft_inset(char a, char *set)
+{
+	int i;
+
+	i = 0;
+	while (set[i])
+	{
+		if (set[i] == a)
+			return (1);
+		i++;
+	}
+	return (0);
+}
+char *ft_strtrim(char const *s1, char const *set)
+{
+	int i;
+	int start;
+	int end;
+	char *str;
+
+	i = 0;
+	start = 0;
+	end = ft_strlen(s1) - 1;
+	if (!s1)
+		return (NULL);
+	if (!set)
+		return (ft_strdup(s1));
+	while (s1[start] && ft_inset(s1[start], set))
+		start++;
+	while (end >= 0 && ft_inset(s1[end], set))
+		end--;
+	if (start > end)
+		return (ft_strdup(""));
+	str = malloc(sizeof(char) * (end - start + 2));
+	if (!str)
+		return (NULL);
+	while (start <= end)
+		str[i++] = s1[start++];
+	str[i] = '\0';
+	return (str);
+}
