c376ca02f3775e9a1f81cf410511fd6cbb32ea14
Matteo <229449054+chauvinmatteo@users.noreply.github.com>
2025-11-11 14:37:37 +0100

    Added ft_itoa.c + itoa in Makefile

--- ft_itoa.c
+++ ft_itoa.c
@@ -5,14 +5,68 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: mchauvin <mchauvin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/11/11 11:51:43 by mchauvin          #+#    #+#             */
-/*   Updated: 2025/11/11 11:54:33 by mchauvin         ###   ########.fr       */
+/*   Created: 2025/11/11 13:56:36 by mchauvin          #+#    #+#             */
+/*   Updated: 2025/11/11 14:35:57 by mchauvin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char *ft_itoa(int n)
+static int	ft_nbrlen(long int i)
 {
-	
+	int	len;
+
+	len = 0;
+	if (i == 0)
+		return (1);
+	if (i < 0)
+	{
+		i = -i;
+		len++;
+	}
+	while (i > 0)
+	{
+		i = i / 10;
+		len++;
+	}
+	return (len);
 }
+
+char	*ft_itoa(int n)
+{
+	char		*s;
+	long int	nb;
+	int			i;
+
+	s = malloc(sizeof(char) * ft_nbrlen(n) + 1);
+	if (!s)
+		return (NULL);
+	s[ft_nbrlen(n)] = '\0';
+	if (n < 0)
+		nb = -(long int)n;
+	else
+		nb = n;
+	i = ft_nbrlen(n) - 1;
+	if (n == 0)
+		s[0] = '0';
+	while (nb > 0)
+	{
+		s[i] = (nb % 10) + '0';
+		nb = nb / 10;
+		i--;
+	}
+	if (n < 0)
+		s[0] = '-';
+	return (s);
+}
+/*#include <stdio.h>
+
+int	main(void)
+{
+	printf("%s\n", ft_itoa(0));
+	printf("%s\n", ft_itoa(144));
+	printf("%s\n", ft_itoa(-144));
+	printf("%s\n", ft_itoa(-2147483648));
+	printf("%s\n", ft_itoa(2147483647));
+	return (0);
+}*/
