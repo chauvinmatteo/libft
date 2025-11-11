b51dbbe29ff4cea4a1d534da9b170d1607b6cf65
Matteo <229449054+chauvinmatteo@users.noreply.github.com>
2025-11-11 12:27:08 +0100

    ft_split

--- ft_itoa.c
+++ ft_itoa.c
@@ -1,42 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mchauvin <mchauvin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/11/05 10:00:32 by chauv             #+#    #+#             */
-/*   Updated: 2025/11/11 11:49:34 by mchauvin         ###   ########.fr       */
+/*   Created: 2025/11/11 11:51:43 by mchauvin          #+#    #+#             */
+/*   Updated: 2025/11/11 11:54:33 by mchauvin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memmove(void *dest, const void *src, size_t n)
+char *ft_itoa(int n)
 {
-	size_t			i;
-	unsigned char	*d;
-	unsigned char	*s;
-
-	d = (unsigned char *)dest;
-	s = (unsigned char *)src;
-	if (n == 0 || !src)
-		return (dest);
-	if (dest > src)
-	{
-		d += n - 1;
-		s += n - 1;
-		while (n--)
-			*d-- = *s--;
-	}
-	else
-	{
-		i = 0;
-		while (i < n)
-		{
-			d[i] = s[i];
-			i++;
-		}
-	}
-	return (dest);
+	
 }
--- ft_split.c
+++ ft_split.c
@@ -6,9 +6,23 @@
 /*   By: mchauvin <mchauvin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/10 15:59:47 by mchauvin          #+#    #+#             */
-/*   Updated: 2025/11/10 16:09:04 by mchauvin         ###   ########.fr       */
+/*   Updated: 2025/11/11 11:48:42 by mchauvin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
+
+static int	ft_nextword(const char *s, int *i, int *start, char c)
+{
+	while (s[*i] == c)
+		(*i)++;
+	if (!s[*i])
+		return (0);
+	*start = *i;
+	while (s[*i] && s[*i] != c)
+		(*i)++;
+	return (1);
+}
 
 static int	ft_countword(const char *str, const char c)
 {
@@ -21,16 +35,96 @@ static int	ft_countword(const char *str, const char c)
 	{
 		while (str[i] == c)
 			i++;
-		if (str[i] != c && (i = 0 || str[i - 1] == c))
+		if (str[i])
 		{
 			count++;
-			i++;
+			while (str[i] && str[i] != c)
+				i++;
 		}
 	}
 	return (count);
 }
 
+static char	*ft_worddup(const char *str, int start, int end)
+{
+	char	*word;
+	int		i;
+
+	i = 0;
+	word = malloc(sizeof(char) * (end - start + 1));
+	if (!word)
+		return (NULL);
+	while (start < end)
+	{
+		word[i] = str[start];
+		i++;
+		start++;
+	}
+	word[i] = '\0';
+	return (word);
+}
+
+static void	ft_frall(char **tab, int i)
+{
+	while (i >= 0)
+	{
+		free(tab[i]);
+		i--;
+	}
+	free(tab);
+}
 
 char	**ft_split(char const *s, char c)
 {
+	int		i;
+	int		j;
+	int		start;
+	int		end;
+	char	**tab;
+
+	if (!s)
+		return (NULL);
+	tab = malloc(sizeof(char *) * (ft_countword(s, c) + 1));
+	if (!tab)
+		return (NULL);
+	j = 0;
+	i = 0;
+	while (ft_nextword(s, &i, &start, c))
+	{
+		end = i;
+		tab[j] = ft_worddup(s, start, end);
+		if (!tab[j])
+			return (ft_frall(tab, j - 1), NULL);
+		j++;
+	}
+	tab[j] = NULL;
+	return (tab);
 }
+/*
+#include <stdio.h>
+
+int	main(void)
+{
+	const char	*str = "Jjqofanlfmafmqsamcamiqngdkfsafalmalsf";
+	const char	sep = 'f';
+	char		**tab;
+	int			i;
+
+	i = 0;
+	tab = ft_split(str, sep);
+	if (!tab)
+		return (1);
+	while (tab[i])
+	{
+		printf("%s\n", tab[i]);
+		i++;
+	}
+	i = 0;
+	while (tab[i])
+	{
+		free(tab[i]);
+		i++;
+	}
+	free(tab);
+	return (0);
+}*/
