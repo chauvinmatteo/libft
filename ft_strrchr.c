c9abdb749fc0c76509e9e5a7765ad38d78b0c1b7
Matteo Chauvin <mchauvin@z2r5p6.42lyon.fr>
2025-11-12 11:03:30 +0100

    corrected strchr and strrchr

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: mchauvin <mchauvin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/05 12:53:35 by mchauvin          #+#    #+#             */
-/*   Updated: 2025/11/10 11:22:21 by mchauvin         ###   ########.fr       */
+/*   Updated: 2025/11/12 11:02:59 by mchauvin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,16 @@
 
 char	*ft_strrchr(const char *str, int to_find)
 {
-	int	i;
+	int				i;
+	unsigned char	tf;
 
 	i = 0;
+	tf = (unsigned char)to_find;
 	while (str[i])
 		i++;
 	while (i >= 0)
 	{
-		if (str[i] == to_find)
+		if ((unsigned char)str[i] == tf)
 			return ((char *)&str[i]);
 		i--;
 	}
