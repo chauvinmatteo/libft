0ee61b378439d3b2995d39ab6c83d9c315d5bdfe
Matteo <229449054+chauvinmatteo@users.noreply.github.com>
2025-11-13 17:10:52 +0100

    Added lstiter and lstmap

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: mchauvin <mchauvin@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2025/11/06 15:31:42 by mchauvin          #+#    #+#              #
-#    Updated: 2025/11/13 11:28:55 by mchauvin         ###   ########.fr        #
+#    Updated: 2025/11/13 12:11:04 by mchauvin         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -28,7 +28,7 @@ SRCS = ft_isalpha.c ft_isdigit.c ft_isalnum.c ft_isascii.c \
 OBJS = $(SRCS:.c=.o)
 
 SRCS_BONUS = ft_lstnew.c ft_lstadd_front.c ft_lstsize.c ft_lstlast.c ft_lstadd_back.c ft_lstdelone.c \
-	ft_lstclear.c
+	ft_lstclear.c ft_lstiter.c ft_lstmap.c
 
 OBJS_BONUS = $(SRCS_BONUS:.c=.o)
 
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: mchauvin <mchauvin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/04 14:18:05 by mchauvin          #+#    #+#             */
-/*   Updated: 2025/11/13 11:28:34 by mchauvin         ###   ########.fr       */
+/*   Updated: 2025/11/13 15:36:55 by mchauvin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,5 +65,8 @@ t_list				*ft_lstlast(t_list *lst);
 void				ft_lstadd_back(t_list **lst, t_list *new);
 void				ft_lstdelone(t_list *lst, void (*del)(void *));
 void				ft_lstclear(t_list **lst, void (*del)(void *));
+void				ft_lstiter(t_list *lst, void (*f)(void *));
+t_list				*ft_lstmap(t_list *lst, void *(*f)(void *),
+						void (*del)(void *));
 
 #endif
